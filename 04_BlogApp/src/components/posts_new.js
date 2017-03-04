acc89df868cfe3eb5c3bebb4f986d614546024ba
Jason Arnold <jason.arnold.00@gmail.com>
2017-03-04 17:58:17 -0500

    add validation to title field

--- 04_BlogApp/src/components/posts_new.js
+++ 04_BlogApp/src/components/posts_new.js
@@ -12,6 +12,9 @@ class PostsNew extends Component {
         <div className="form-group">
           <label>Title</label>
           <input type="text" className="form-control" {...title} />
+          <div className="text-help">
+            {title.touched ? title.error : ''}
+          </div>
         </div>
         <div className="form-group">
           <label>Categories</label>
@@ -27,7 +30,17 @@ class PostsNew extends Component {
   }
 }
 
+function validate(values){
+  const errors = {};
+
+  if(!values.title){
+    errors.title = 'Enter a title';
+  }
+  return errors;
+}
+
 export default reduxForm({
   form: 'PostsNew', //does not need to match the component name
-  fields: ['title', 'categories','content'] //names of the fields on the form
+  fields: ['title', 'categories','content'], //names of the fields on the form
+  validate
 }, null, {createPost})(PostsNew); //this will add a the form to the application state with the contents of the fields
