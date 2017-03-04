b704ff43650babe8a7216d389082ef80eed472d2
Jason Arnold <jason.arnold.00@gmail.com>
2017-03-04 17:41:54 -0500

    add redux form

--- 04_BlogApp/src/actions/index.js
+++ 04_BlogApp/src/actions/index.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 export const FETCH_POST = 'FETCH_POST';
+export const CREATE_POST = 'CREATE_POST';
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
 const API_KEY = '?key=reindeer_flotilla';
 
@@ -11,4 +12,13 @@ export function fetchPosts(){
     type: FETCH_POST,
     payload: request
   };
+}
+
+export function createPosts(){
+  const request = axios.post(`${ROOT_URL}/posts${API_KEY}`, props)
+
+  return {
+    type: CREATE_POST,
+    payload: request
+  }
 }
--- 04_BlogApp/src/components/posts_new.js
+++ 04_BlogApp/src/components/posts_new.js
@@ -1,11 +1,32 @@
 import React, {Component} from 'react';
+import {reduxForm} from 'redux-form';
 
 class PostsNew extends Component {
   render(){
+    const {fields: {title, categories, content}, handleSubmit} = this.props;
+
     return(
-      <div>Create Form</div>
+      <form onSubmit={handleSubmit}>
+        <h3>Create a New Post</h3>
+        <div className="form-group">
+          <label>Title</label>
+          <input type="text" className="form-control" {...title} />
+        </div>
+        <div className="form-group">
+          <label>Categories</label>
+          <input type="text" className="form-control" {...categories} />
+        </div>
+        <div className="form-group">
+          <label>Content</label>
+          <textarea className="form-control" {...content} />
+        </div>
+        <button type="submit" className="btn btn-primary">Submit</button>
+      </form>
     )
   }
 }
 
-export default PostsNew;
+export default reduxForm({
+  form: 'PostsNew', //does not need to match the component name
+  fields: ['title', 'categories','content'] //names of the fields on the form
+})(PostsNew); //this will add a the form to the application state with the contents of the fields
